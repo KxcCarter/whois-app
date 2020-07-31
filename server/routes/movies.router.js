277badcfd62dcfea44fb80eb12e164ded4cf0817
Kenneth Carter <kxccarter@gmail.com>
2020-07-31 16:46:36 -0500

    Builds GET route in movies.router.js

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -4,4 +4,26 @@ const pool = require('../modules/pool');
 
 //  TODO: build routes here
 
+// GET route
+
+router.get('/', (req, res) => {
+  const query = `SELECT * FROM movies;`;
+
+  pool
+    .query(query)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((err) => {
+      console.log('GET error: ', err);
+      res.sendStatus(500);
+    });
+});
+
+// POST route
+
+// PUT route
+
+// DELETE route
+
 module.exports = router;
