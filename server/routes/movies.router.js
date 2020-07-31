3b6b96bc932fdc263671058f5ca176e34e61c68e
Kenneth Carter <kxccarter@gmail.com>
2020-07-31 18:12:24 -0500

    builds GET route for getting specific movies from database via id and req.params. Also creates a GET route for genres.

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -4,9 +4,9 @@ const pool = require('../modules/pool');
 
 //  TODO: build routes here
 
-// GET route
-
-router.get('/', (req, res) => {
+// GET routes
+// movies
+router.get('/movies', (req, res) => {
   const query = `SELECT * FROM movies;`;
 
   pool
@@ -20,7 +20,59 @@ router.get('/', (req, res) => {
     });
 });
 
+// GET specific movie
+router.get('/movies/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM movies WHERE id = $1;`;
+
+  pool
+    .query(query, [id])
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((err) => {
+      console.log('GET error: ', err);
+      res.sendStatus(500);
+    });
+});
+
+// genres
+router.get('/genres', (req, res) => {
+  const query = `SELECT * FROM genres;`;
+
+  pool
+    .query(query)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((err) => {
+      console.log('GET error: ', err);
+      res.sendStatus(500);
+    });
+});
+// END GET ROUTES
+
 // POST route
+
+router.post('/', (req, res) => {
+  const query = `INSERT INTO "movies"
+  ("title", "poster", "description")
+VALUES ($1, $2, $3)`;
+  // TODO: finish skeleton req.body destructuring
+  const movieTitle = req.body;
+  const moviePoster = req.body;
+  const movieDescription = req.body;
+
+  pool
+    .query(query, [])
+    .then((dbRes) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('POST error: ', err);
+      res.sendStatus(500);
+    });
+});
 
 // PUT route
 
