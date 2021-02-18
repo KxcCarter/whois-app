26a78b92adce13f07a80a322097c41f23ec35e25
Kenneth Carter <kxccarter@gmail.com>
2021-02-17 22:05:22 -0600

    added PORT to environment variables.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const whoIsAPI = require('./routers/who-is-API.router');
 
 const server = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
